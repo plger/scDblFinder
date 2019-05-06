3dcc47d6c0456a670e2d92d69bd74b4c2ce3de20
Pierre-Luc Germain <pl.germain@gmail.com>
2019-05-06 12:49:49 +0200

    fixed missing param in call to thresholding function

--- R/doubletFinder.R
+++ R/doubletFinder.R
@@ -87,7 +87,7 @@ plDoubletFinder <- function( sce, artificialDoublets=NULL, clusters=NULL, minClu
   d$type <- ctype
   
   if(verbose) message("Finding threshold...")
-  th <- doubletThresholding(d$ratio, dbr, dbr.sd, clusters, do.plot=verbose)
+  th <- doubletThresholding(d$ratio, d$type, dbr, dbr.sd, clusters, do.plot=verbose)
   if(verbose) message("Threshold found:", round(th,3))
   d$classification <- ifelse(d$ratio >= th, "doublet", "singlet")
   if(fullTable) return(d)
