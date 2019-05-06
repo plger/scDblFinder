166e53e2bed9d337a549ba7fdc18cdbbe9e8db83
Pierre-Luc Germain <pl.germain@gmail.com>
2019-05-06 12:46:00 +0200

    split thresholding into separate function

--- R/doubletFinder.R
+++ R/doubletFinder.R
@@ -1,7 +1,8 @@
 #' plDoubletFinder
 #' 
 #' An approach similar to `DoubletFinder` for identifying doublets, but much 
-#' simpler/faster and independent from normalization (executed directly on counts).
+#' simpler/faster, independent from normalization (executed directly on counts), and
+#' arguably more accurate.
 #'
 #' @param sce An object of class `SingleCellExperiment`
 #' @param artificialDoublets The approximate number of artificial doublets to create. If
@@ -10,23 +11,21 @@
 #' This is used to make doublets more efficiently.
 #' @param minClusSize The minimum cluster size for `quickCluster` (default 50)
 #' @param d The number of dimensions used to build the KNN network (default 10)
-#' @param dbr The expected doublet rate, defaults to 0.021 on the basis of the 
-#' on the basis of the mixology 10x datasets results, where the proportion of
-#' demuxlet doublet is estimated respectively at 0.012 and 0.029 (after accounting
-#' for homotypic doublets).
+#' @param dbr The expected doublet rate, see `doubletThresholding` for more information.
 #' @param dbr.sd The standard deviation of the doublet rate, defaults to 0.01.
 #' @param graph.type Either 'snn' or 'knn' (default).
 #' @param fullTable Logical; whether to return the full table including artificial 
 #' doublets (default FALSE), rather than the table for real cells only.
 #' @param verbose Logical; whether to print messages and the thresholding plot (default
 #' TRUE).
-#' @param ... arguments passed to `scran::buildSNNGraph`; can for instance be used to
-#' pass `BPPARAM` and speed up the graph building.
+#' @param ... arguments passed to `scran::build*NNGraph`; can for instance be used to
+#' pass `BPPARAM` and speed up the graph building through multithreading.
 #'
-#' @return A data.frame including, for each cells, the number of neighbors considered,
-#' the number of these neighbors that are from artificial doublets, the ratio of 
-#' neighbors that are from artificial doublets, the enrichment in artificial doublets,
-#'  and the classification (after applying the threshold).
+#' @return A data.frame including, for each cells (rows, in the order in which they appear
+#' in the `sce` object), the number of neighbors considered, the number of these neighbors
+#'  that are from artificial doublets, the ratio of neighbors that are from artificial 
+#'  doublets (which represents a quantitative doublet the score), the enrichment in 
+#'  artificial doublets, and the classification (after applying the threshold).
 #' 
 #' @export
 plDoubletFinder <- function( sce, artificialDoublets=NULL, clusters=NULL, minClusSize=50,
@@ -88,40 +87,7 @@ plDoubletFinder <- function( sce, artificialDoublets=NULL, clusters=NULL, minClu
   d$type <- ctype
   
   if(verbose) message("Finding threshold...")
-  # adjust expected doublet proportion for expected homotypic doublets
-  homotypic.prop <- sum((table(clusters)/length(clusters))^2)
-  dbr <- dbr*(1-homotypic.prop)
-  f1 <- ecdf(d$ratio[which(d$type!="artificial")])
-  f2 <- ecdf(d$ratio[which(d$type=="artificial")])
-  # accuracy
-  accfn <- function(x){ (1-f1(x))+max(0,f2(x)-homotypic.prop) }
-  accfn2 <- function(x){ (1-f1(x))+f2(x) }
-  # deviation from expected doublet proportion
-  dbr.dev <- function(x){
-    p <- sum(d$ratio>=x & d$type=="real")/ncol(sce)
-    abs(pcauchy(abs(dbr-p), scale=dbr.sd)*2-1)^2
-  }
-  totfn <- function(x){
-    accfn(x)+dbr.dev(x)
-  }
-  th <- optimize(totfn, c(0,1), maximum=FALSE)$minimum
-  if(verbose){
-    # we plot the thresholding data
-    x <- 1:99/100
-    acc <- sapply(x, accfn)
-    plot(x, acc, type="l", lty="dashed", col="blue", ylim=c(0,max(acc)), lwd=2,
-         main="Thresholding", ylab="Error rate", 
-         xlab="Ratio of artificial doublets in neighborhood")
-    lines(x, sapply(x, accfn2), col="blue", lwd=2)
-    lines(x, sapply(x, dbr.dev), col="red", lwd=2)
-    lines(x, sapply(x, FUN=function(x) sum(d$ratio>=x & d$type=="real")/ncol(sce)), col="darkgrey")
-    abline(v=th, lty="dashed")
-    leg <- c( "Accuracy in classifying real cells vs artificial doublets",
-              "Accuracy in classifying artificial doublets, adjusted for homotypy",
-              "Deviation from expected doublet rate",
-              "Proportion of real cells considered doublet" )
-    legend("top", col=c("blue","blue", "red","darkgrey"), lty=c(1,2,1,1), lw=2, legend=leg)
-  }
+  th <- doubletThresholding(d$ratio, dbr, dbr.sd, clusters, do.plot=verbose)
   if(verbose) message("Threshold found:", round(th,3))
   d$classification <- ifelse(d$ratio >= th, "doublet", "singlet")
   if(fullTable) return(d)
---
+++ R/doubletThresholding.R
@@ -0,0 +1,73 @@
+
+#' doubletThresholding
+#'
+#' 
+#'
+#' @param scores A vector of the doublet score for each cell (real and artificial); can be
+#' anything ranging from 0 to 1, with higher scores indicating higher change of being a 
+#' doublet.
+#' @param celltypes A vector of the same length as `scores` indicating, for each cell,
+#' whether it is a 'real' cell or an 'artificial' doublet. Missing values not allowed.
+#' @param dbr The expected (mean) doublet rate, defaults to 0.021 on the basis of the 
+#'  mixology 10x datasets demuxlet results, where the proportion of demuxlet doublet is
+#'  estimated respectively at 0.012 and 0.029 (after accounting adding expected homotypic 
+#'  doublets).
+#' @param dbr.sd The deviation of the doublet rate, representing the uncertainty in the 
+#' estimate. Effectively the scale of a Cauchy distribution. Defaults to 0.01.
+#' @param clusters Optional vector of cluster assignment for each (real) cell, used for
+#' homotypic doublet correction.
+#' @param do.plot Logical; whether to plot the thresholding data (default TRUE).
+#'
+#' @return A scaler indicating the decided threshold.
+#' @export
+doubletThresholding <- function(scores, celltypes, dbr=0.021, dbr.sd=0.01, clusters=NULL, do.plot=TRUE){
+  if(!all(sort(unique(celltypes))==c("artificial","real"))){
+    stop("`celltypes` should be either 'real' or 'artificial'.")
+  }
+  if(any(is.na(scores))){
+    w <- which(is.na(scores))
+    scores <- scores[-w]
+    celltypes <- celltypes[-w]
+    warning(length(w)," cells with NA scores were discarded.")
+  }
+  # if clusters given, adjust expected doublet proportion for expected homotypic doublets
+  homotypic.prop <- ifelse(is.null(clusters), 0, sum((table(clusters)/length(clusters))^2))
+  dbr <- dbr*(1-homotypic.prop)
+  f1 <- ecdf(scores[which(celltypes!="artificial")])
+  f2 <- ecdf(scores[which(celltypes=="artificial")])
+  # accuracy functions
+  accfn <- function(x){ (1-f1(x))+max(0,f2(x)-homotypic.prop) }
+  accfn2 <- function(x){ (1-f1(x))+f2(x) }
+  # deviation from expected doublet proportion
+  dbr.dev <- function(x){
+    p <- sum(scores>=x & celltypes=="real")/sum(celltypes=="real")
+    abs(pcauchy(abs(dbr-p), scale=dbr.sd)*2-1)^2
+  }
+  totfn <- function(x){
+    accfn(x)+dbr.dev(x)
+  }
+  th <- optimize(totfn, c(0,1), maximum=FALSE)$minimum
+  if(do.plot){
+    # we plot the thresholding data
+    x <- 1:99/100
+    acc <- sapply(x, accfn)
+    plot(x, acc, type="l", lty="dashed", col="blue", ylim=c(0,max(acc)), lwd=2,
+         main="Thresholding", ylab="Error rate", 
+         xlab="Ratio of artificial doublets in neighborhood")
+    if(!is.null(clusters)) lines(x, sapply(x, accfn2), col="blue", lwd=2)
+    lines(x, sapply(x, dbr.dev), col="red", lwd=2)
+    lines(x, sapply(x, FUN=function(x) sum(scores>=x & celltypes=="real")/sum(celltypes=="real")), col="darkgrey")
+    abline(v=th, lty="dashed")
+    leg <- c( "Accuracy in classifying real cells vs artificial doublets",
+              "Accuracy in classifying artificial doublets, adjusted for homotypy",
+              "Deviation from expected doublet rate",
+              "Proportion of real cells considered doublet" )
+    if(is.null(clusters)){
+      leg <- leg[-2]
+      legend("top", col=c("blue", "red","darkgrey"), lty=c(1,1,1), lw=2, legend=leg)
+    }else{
+      legend("top", col=c("blue","blue", "red","darkgrey"), lty=c(1,2,1,1), lw=2, legend=leg)
+    }
+    th
+  }
+}
