378c9e7e720f5e4bc1a567d634f70c514a0aa8b4
Pierre-Luc Germain <pl.germain@gmail.com>
2019-08-29 18:07:15 +0200

    fixed return bug when not plotting

--- R/doubletThresholding.R
+++ R/doubletThresholding.R
@@ -1,24 +1,25 @@
 #' doubletThresholding
 #'
-#' @param scores A vector of the doublet score for each cell (real and artificial); can be
-#' anything ranging from 0 to 1, with higher scores indicating higher change of being a 
-#' doublet.
-#' @param celltypes A vector of the same length as `scores` indicating, for each cell,
-#' whether it is a 'real' cell or an 'artificial' doublet. Missing values not allowed.
-#' @param clusters Optional vector of cluster assignment for each (real) cell, used for
-#' homotypic doublet correction.
+#' @param scores A vector of the doublet score for each cell (real and 
+#' artificial); can be anything ranging from 0 to 1, with higher scores 
+#' indicating higher change of being a doublet.
+#' @param celltypes A vector of the same length as `scores` indicating, for each
+#'  cell, whether it is a 'real' cell or an 'artificial' doublet. Missing values
+#'   not allowed.
+#' @param clusters Optional vector of cluster assignment for each (real) cell, 
+#' used for homotypic doublet correction.
 #' @param dbr The expected (mean) doublet rate.
-#' @param dbr.sd The standard deviation of the doublet rate, representing the uncertainty
-#'  in the estimate.
-#' @param prop.fullyRandom The proportion of artificical doublets that are fully random,
-#' used for homotypy correction. Default 0.25 (the default value in 
+#' @param dbr.sd The standard deviation of the doublet rate, representing the 
+#' uncertainty in the estimate.
+#' @param prop.fullyRandom The proportion of artificical doublets that are fully
+#'  random, used for homotypy correction. Default 0.25 (the default value in 
 #' `getArtificialDoublets`). Ignored if `clusters=NULL`
 #' @param do.plot Logical; whether to plot the thresholding data (default TRUE).
 #'
 #' @return A scaler indicating the decided threshold.
 #' @export
-doubletThresholding <- function(scores, celltypes, clusters=NULL, dbr=0.025, dbr.sd=0.02, 
-                                prop.fullyRandom=0, do.plot=TRUE){
+doubletThresholding <- function(scores, celltypes, clusters=NULL, dbr=0.025, 
+                                dbr.sd=0.02, prop.fullyRandom=0, do.plot=TRUE){
   if(!all(sort(unique(celltypes))==c("artificial","real"))){
     stop("`celltypes` should be either 'real' or 'artificial'.")
   }
@@ -33,7 +34,7 @@ doubletThresholding <- function(scores, celltypes, clusters=NULL, dbr=0.025, dbr
   }else{
     homotypic.prop <- 0
   }
-  # if clusters given, adjust expected doublet proportion for expected homotypic doublets
+  # if clusters given, adjust expected dbr for expected homotypic doublets
   dbr <- dbr*(1-homotypic.prop)
   f1 <- ecdf(scores[which(celltypes!="artificial")])
   f2 <- ecdf(scores[which(celltypes=="artificial")])
@@ -53,12 +54,14 @@ doubletThresholding <- function(scores, celltypes, clusters=NULL, dbr=0.025, dbr
     # we plot the thresholding data
     x <- 1:99/100
     acc <- sapply(x, accfn)
-    plot(x, acc, type="l", lty=ifelse(is.null(clusters),1,2), col="blue", ylim=c(0,max(acc)), lwd=2,
-         main="Thresholding", ylab="Proportion", 
+    plot(x, acc, type="l", lty=ifelse(is.null(clusters),1,2), col="blue", 
+         ylim=c(0,max(acc)), lwd=2, main="Thresholding", ylab="Proportion", 
          xlab="Ratio of artificial doublets in neighborhood")
     if(!is.null(clusters)) lines(x, sapply(x, accfn2), col="blue", lwd=2)
     lines(x, sapply(x, dbr.dev), col="red", lwd=2)
-    lines(x, sapply(x, FUN=function(x) sum(scores>=x & celltypes=="real")/sum(celltypes=="real")), col="darkgrey")
+    lines(x, sapply(x, FUN=function(x){
+            sum(scores>=x & celltypes=="real")/sum(celltypes=="real")
+        }), col="darkgrey")
     abline(v=th, lty="dashed")
     leg <- c( "Error in classifying real cells vs artificial doublets",
               "Error in classifying artificial doublets, adjusted for homotypy",
@@ -66,10 +69,12 @@ doubletThresholding <- function(scores, celltypes, clusters=NULL, dbr=0.025, dbr
               "Proportion of real cells considered doublet" )
     if(is.null(clusters)){
       leg <- leg[-2]
-      legend("top", col=c("blue", "red","darkgrey"), lty=c(1,1,1), lw=2, legend=leg)
+      legend("top", col=c("blue", "red","darkgrey"), 
+             lty=c(1,1,1), lw=2, legend=leg)
     }else{
-      legend("top", col=c("blue","blue", "red","darkgrey"), lty=c(1,2,1,1), lw=2, legend=leg)
+      legend("top", col=c("blue","blue", "red","darkgrey"), 
+             lty=c(1,2,1,1), lw=2, legend=leg)
     }
-    th
   }
+  th
 }
