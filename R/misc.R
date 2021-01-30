871860b90111159eaa0a295cd7dd6c76901bc2fa
plger <pl.germain@gmail.com>
2021-01-30 17:24:46 +0100

    replace benchmark figure

--- R/misc.R
+++ R/misc.R
@@ -119,11 +119,16 @@ selFeatures <- function(sce, clusters=NULL, nfeatures=1000, propMarkers=0, FDR.m
       g <- row.names(sce)[order(Matrix::rowMeans(counts(sce),na.rm=TRUE),
                                 decreasing=TRUE)[seq_len(ng)]]
     }else{
-      cl.means <- as.matrix(assay(scuttle::sumCountsAcrossCells(counts(sce), clusters)))
-      g <- unique(as.numeric(t(apply(cl.means, 2, FUN=function(x){
-    order(x, decreasing=TRUE)[seq_len(nfeatures)]
-  }))))[seq_len(ng)]
-      g <- row.names(sce)[g]
+      g <- tryCatch({
+        cl.means <- as.matrix(assay(scuttle::sumCountsAcrossCells(counts(sce), clusters)))
+        g <- unique(as.numeric(t(apply(cl.means, 2, FUN=function(x){
+          order(x, decreasing=TRUE)[seq_len(nfeatures)]
+        }))))[seq_len(ng)]
+        row.names(sce)[g]
+      }, error=function(e){
+        g <- row.names(sce)[order(Matrix::rowMeans(counts(sce),na.rm=TRUE),
+                                  decreasing=TRUE)[seq_len(ng)]]
+      })
     }
   }
   if(ng==nfeatures) return(g)
@@ -429,4 +434,91 @@ propHomotypic <- function(clusters){
     d[[newName]][w] <- thm + (d[[what]][w]-ths[w])*(1-thm)/(1-ths[w])
   }
   return(d)
+}
+
+#' aggregateFeatures
+#'
+#' Aggregates similar features (rows).
+#'
+#' @param x A integer/numeric (sparse) matrix, or a `SingleCellExperiment`
+#' including a `counts` assay.
+#' @param dims.use The PCA dimensions to use for clustering rows.
+#' @param k The approximate number of meta-features desired
+#' @param num_init The number of initializations used for k-means clustering.
+#' @param use.mbk Logical; whether to use minibatch k-means (see
+#' \code{\link[mbkmeans]{mbkmeans}}). If NULL, the minibatch approach will be
+#' used if there are more than 30000 features.
+#' @param use.subset How many cells (columns) to use to cluster the features.
+#' @param use.TFIDF Logical; whether to use \link{TFIDF} normalization (instead
+#' of standard normalization) to assess the similarity between features.
+#' similarity
+#' @param ... Passed to \code{\link[mbkmeans]{mbkmeans}}. Can for instance be
+#' used to pass the `BPPARAM` argument for multithreading.
+#'
+#' @return An aggregated version of `x` (either an array or a
+#' `SingleCellExperiment`, depending on the input).
+#'
+#' @importFrom scuttle logNormCounts
+#' @importFrom BiocSingular runPCA IrlbaParam
+aggregateFeatures <- function(x, dims.use=seq(2L,12L), k=1000, num_init=2,
+                               use.mbk=NULL, use.subset=5000, use.TFIDF=TRUE,
+                              ...){
+  xo <- x
+  if(ncol(x)>use.subset){
+    if(is(x,"SingleCellExperiment")){
+      cs <- Matrix::colSums(counts(x))
+    }else{
+      cs <- Matrix::colSums(x)
+    }
+    x <- x[,order(cs,decreasing=TRUE)[seq_len(use.subset)]]
+  }
+  if(use.TFIDF){
+    if(is(x,"SingleCellExperiment")) x <- counts(x)
+    x <- TFIDF(x)
+  }else{
+    if(is(x,"SingleCellExperiment")){
+      if("logcounts" %in% assayNames(x)) x <- scuttle::logNormCounts(x)
+      x <- logcounts(x)
+    }
+    x <- t(normalizeCounts(t(x)))
+  }
+  pca <- runPCA(x, BSPARAM=IrlbaParam(), center=FALSE,
+                rank=max(dims.use))$x[,dims.use]
+  if(is.null(use.mbk)) use.mbk <- nrow(x) > 30000
+  if(use.mbk && suppressWarnings(require("mbkmeans", quietly=TRUE))){
+    fc <- mbkmeans:::mbkmeans(t(pca), k, num_init=num_init, ...)$Clusters
+  }else{
+    fc <- kmeans(pca, k, nstart=num_init, iter.max=100)$cluster
+  }
+  x <- scuttle::sumCountsAcrossFeatures(xo, fc)
+  row.names(x) <- paste0("feat",seq_len(nrow(x)))
+  if(is(xo,"SingleCellExperiment")){
+    x <- SingleCellExperiment(list(counts=x), colData=colData(xo),
+                              reducedDims=reducedDims(xo))
+  }
+  x
+}
+
+#
+#' TFIDF
+#'
+#' The Term Frequency - Inverse Document Frequency (TF-IDF) normalization, as
+#' implemented in Stuart & Butler et al. 2019.
+#'
+#' @param x The matrix of occurrences
+#' @param sf Scaling factor
+#'
+#' @return An array of same dimensions as `x`
+#' @export
+#'
+#' @examples
+#' m <- matrix(rpois(500,1),nrow=50)
+#' m <- TFIDF(m)
+TFIDF <- function(x, sf=10000){
+  if(!is(x,"sparseMatrix")) x <- as(x, "sparseMatrix")
+  tf <- Matrix::tcrossprod(x, Diagonal(x=1L/Matrix::colSums(x)))
+  idf <- ncol(x)/Matrix::rowSums(x)
+  x <- log1p(sf*(Diagonal(length(idf), x=idf) %*% tf))
+  x[is.na(x)] <- 0
+  x
 }
